72b0511a5c62fdd42e2c6c40a8359764f9eaa19c
Andrew Welch <andrew@nystudio107.com>
2023-01-07 16:35:42 -0500

    Initial Commit

---
+++ Toolbar16/Toolbar16App.swift
@@ -0,0 +1,17 @@
+//
+//  Toolbar16App.swift
+//  Toolbar16
+//
+//  Created by Andrew Welch on 1/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Toolbar16App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
